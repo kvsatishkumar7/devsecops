9dc58e99df8393c9ceaa36b9e91ba5a26ccc8c9a
Satish <kvsatishkumar7@gmail.com>
2025-10-05 12:18:30 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+backend "s3" {
+    bucket = "terraformstatebucket05102025"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+    use_lockfile = true
+  }
