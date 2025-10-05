bdf4e1f45826f53922cb26cc831ce1367ceb4aa0
Satish <kvsatishkumar7@gmail.com>
2025-10-05 21:16:59 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,9 +5,13 @@ terraform {
       version = "~> 5.92"
     }
   }
-backend "s3" {
-    bucket = "terraformstatebucket05102025"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
+  backend "s3" {
+    bucket       = "terraformstatebucket05102025"
+    key          = "terraform.tfstate"
+    region       = "us-west-2"
     use_lockfile = true
   }
+
+
+  required_version = ">= 1.2"
+}
