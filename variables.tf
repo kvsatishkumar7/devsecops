9a018fb7bc34ecd2cc498a4298015e69b613715a
Satish <kvsatishkumar7@gmail.com>
2025-10-06 21:43:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  aws_instance     = "learn-terraform"
+  default     = "learn-terraform"
 }
